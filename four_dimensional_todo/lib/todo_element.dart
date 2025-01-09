cda19b7ec0e3dfe08482578b91a250db41412ee1
Jeremias Bub <jeremias.bub@stud.h-da.de>
2025-01-09 20:32:37 +0100

    nicer todo-cards

--- four_dimensional_todo/lib/todo_element.dart
+++ four_dimensional_todo/lib/todo_element.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class TodoElement {
   String title;
   String? description;
@@ -7,6 +9,19 @@ class TodoElement {
   EisenhowerMatrixCategory eisenhowerMatrixCategory;
 
   TodoElement({required this.title, required this.description, this.done = false, required this.eisenhowerMatrixCategory, required this.dueDate, required this.creationDate});
+
+  Widget getIcon(){
+    switch (eisenhowerMatrixCategory) {
+      case EisenhowerMatrixCategory.urgentImportant:
+        return const Icon(Icons.star);
+      case EisenhowerMatrixCategory.urgentNotImportant:
+        return const Icon(Icons.star_border);
+      case EisenhowerMatrixCategory.notUrgentImportant:
+        return const Icon(Icons.star_half);
+      case EisenhowerMatrixCategory.notUrgentNotImportant:
+        return const Icon(Icons.sunny);
+    }
+  }
 }
 
 enum EisenhowerMatrixCategory {
---
+++ four_dimensional_todo/lib/widgets/todo_card.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:four_dimensional_todo/todo_element.dart';
+
+class TodoCard extends StatefulWidget {
+  final TodoElement todoElement;
+
+  const TodoCard({super.key, required this.todoElement});
+
+  @override
+  State<TodoCard> createState() => _TodoCardState();
+
+  void onDone() {
+    todoElement.done = !todoElement.done;
+  }
+
+  void onArchive() {
+    //TODO: implement onArchive
+  }
+
+  void onDelete() {
+    //TODO: implement onDelete
+  }
+}
+
+class _TodoCardState extends State<TodoCard> {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    var des = '';
+    if (widget.todoElement.description != null) {
+      des = widget.todoElement.description!;
+    }
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Dismissible(
+        key: Key(widget.todoElement.title),
+        onDismissed: (DismissDirection dir) {
+          setState(() {
+            dir == DismissDirection.endToStart
+                ? widget.onDelete()
+                : widget.onArchive();
+          });
+        },
+        background: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            color: Colors.red,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Icon(Icons.delete),
+            ),
+          ),
+        ),
+        secondaryBackground: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            alignment: Alignment.centerRight,
+            color: Colors.green,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Icon(Icons.archive_outlined),
+            ),
+          ),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            color: theme.colorScheme.inversePrimary,
+            child: ListTile(
+              title: Text(widget.todoElement.title),
+              subtitle: Text(des),
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: widget.todoElement.getIcon(),
+              ),
+              trailing: IconButton(
+                icon: widget.todoElement.done
+                    ? Icon(Icons.check)
+                    : Icon(Icons.check_box_outline_blank),
+                onPressed: () {
+                  setState(() {
+                    widget.onDone();
+                  });
+                },
+              ),
+              onTap: () {
+                setState(() {
+                  widget.onDone();
+                });
+              }
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
