463f7f6ff8476e756f8ebddd1ecab8ca2d972688
Jeremias Bub <jeremias.bub@stud.h-da.de>
2025-01-10 11:51:59 +0100

    simplified widget

--- four_dimensional_todo/lib/widgets/todo_card.dart
+++ four_dimensional_todo/lib/widgets/todo_card.dart
@@ -32,18 +32,18 @@ class _TodoCardState extends State<TodoCard> {
     }
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Dismissible(
-        key: Key(widget.todoElement.title),
-        onDismissed: (DismissDirection dir) {
-          setState(() {
-            dir == DismissDirection.endToStart
-                ? widget.onDelete()
-                : widget.onArchive();
-          });
-        },
-        background: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Dismissible(
+          key: Key(widget.todoElement.title),
+          onDismissed: (DismissDirection dir) {
+            setState(() {
+              dir == DismissDirection.endToStart
+                  ? widget.onDelete()
+                  : widget.onArchive();
+            });
+          },
+          background: Container(
             alignment: Alignment.centerLeft,
             color: Colors.red,
             child: Padding(
@@ -51,10 +51,7 @@ class _TodoCardState extends State<TodoCard> {
               child: const Icon(Icons.delete),
             ),
           ),
-        ),
-        secondaryBackground: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
+          secondaryBackground: Container(
             alignment: Alignment.centerRight,
             color: Colors.green,
             child: Padding(
@@ -62,34 +59,32 @@ class _TodoCardState extends State<TodoCard> {
               child: const Icon(Icons.archive_outlined),
             ),
           ),
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-            color: theme.colorScheme.inversePrimary,
-            child: ListTile(
-              title: Text(widget.todoElement.title),
-              subtitle: Text(des),
-              leading: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: widget.todoElement.getIcon(),
-              ),
-              trailing: IconButton(
-                icon: widget.todoElement.done
-                    ? Icon(Icons.check)
-                    : Icon(Icons.check_box_outline_blank),
-                onPressed: () {
-                  setState(() {
-                    widget.onDone();
-                  });
-                },
-              ),
-              onTap: () {
+          child: ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            tileColor: theme.colorScheme.inversePrimary,
+            title: Text(widget.todoElement.title),
+            subtitle: Text(des),
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: widget.todoElement.getIcon(),
+            ),
+            trailing: IconButton(
+              icon: widget.todoElement.done
+                  ? Icon(Icons.check)
+                  : Icon(Icons.check_box_outline_blank),
+              onPressed: () {
                 setState(() {
                   widget.onDone();
                 });
-              }
+              },
             ),
+            onTap: () {
+              setState(() {
+                widget.onDone();
+              });
+            }
           ),
         ),
       ),
